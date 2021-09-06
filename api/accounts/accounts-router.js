e8134bcc344c37b38d53b7954e9c72ef406f2ef9
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-09-06 11:35:45 -0600

    Endpoints wired.

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,51 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json([{}, {}]);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 module.exports = router;
